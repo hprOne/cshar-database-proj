bc59b31e7828068fddbb143660a70aebfab4c5f7
hprOne <bartek.laka@gmail.com>
2021-02-10 20:41:12 +0100

    Add some connections with database

---
+++ cshar-database-proj/AddCarWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using cshar_database_proj;
+
+namespace QuickCar
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddCarWindow.xaml
+    /// </summary>
+    public partial class AddCarWindow : Window
+    {
+        SQL_QuickCarEntities db = new SQL_QuickCarEntities();
+        public AddCarWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_ExitNewCar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_SaveNewCar_Click(object sender, RoutedEventArgs e)
+        {
+            Cars newCar = new Cars()
+            {
+                NameCar = AddCar_Name.Text,
+                YearCar = int.Parse(AddCar_Year.Text),
+            };
+            db.Cars.Add(newCar);
+            db.SaveChanges();          
+            this.Close();
+        }
+    }
+}
--- cshar-database-proj/MainWindow.xaml.cs
+++ cshar-database-proj/MainWindow.xaml.cs
@@ -21,29 +21,28 @@ namespace QuickCar
     /// </summary>
     public partial class MainWindow : Window
     {
+        private void LoadTable()
+        {
+            using (var context = new SQL_QuickCarEntities())
+            {
+                //var StringNameCar = context.Cars.Select(x => x.NameCar);
+                //int StringNameCarLength = StringNameCar.Count();
+
+                //ListBox_Cars.Items.Clear();
+                ////for (int i = 0; i < StringNameCarLength; i++)
+                //foreach (var element in StringNameCar)
+                //{
+                //    ListBox_Cars.Items.Add(element);
+                //}
+                this.ListBox_Cars.ItemsSource = context.Cars.Select(x => x.NameCar).ToList();
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
-            using (var context = new SQL_QuickCarEntities())
-            {
-                var StringNameCar = (from Cars in context.Cars
-                                     select Cars.NameCar)
-                                    .ToArray();
-                int StringNameCarLength = StringNameCar.Count();
-
-                for (int i = 0; i<StringNameCarLength; i++)
-                {
-                    ListBox_Cars.Items.Add(StringNameCar[i]);
-                }
-                
-
-
-                //ListBox_Cars.Items.Add = StringNameCar[0];
-                                     //.ToArray();
-                //textBlok.Text = string.Join(",", StringNameCar);                                
-            }
-            
+            LoadTable();
         }
+        /*
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             using (var context = new SQL_QuickCarEntities())
@@ -57,9 +56,28 @@ namespace QuickCar
                 //textBlok.Text = StringNameCar;
             }
         }
+        */
         private void Button_EndProgram2(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+        private void Button_AddCar(object sender, RoutedEventArgs e)
+        {
+            AddCarWindow addCarWindow = new AddCarWindow();
+            addCarWindow.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource carsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("carsViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // carsViewSource.Źródło = [ogólne źródło danych]
+        }
+
+        private void Button_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            LoadTable();
         }
     }
 }
